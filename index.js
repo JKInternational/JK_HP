ef6b24134e97ea39f1580780faee1573a14f86c2
JKInternational <93498814+JKInternational@users.noreply.github.com>
2021-11-19 17:34:33 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Introduce from "./components/Introduce";
+import Tichop from "./components/Tichop.js";
+import Worthytool from "./components/Worthytool.js";
+import Blueshark from "./components/Blueshark";
+import Etc from "./components/Etc";
+import Movie from "./components/Movie";
+import Notice from "./components/Notice";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+
+
+function App(){
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/"><Main /></Route>
+        <Route path="/introduce"><Introduce /></Route>
+        <Route path="/tichop"><Tichop /></Route>
+        <Route path="/blueshark"><Blueshark /></Route>
+        <Route path="/worthytool"><Worthytool /></Route>
+        <Route path="/etc"><Etc /></Route>
+        <Route path="/movie"><Movie /></Route>
+        <Route path="/notice"><Notice /></Route>
+        <Route path="/contact"><Contact /></Route>
+        <Route path="/"><h1>정확히 좀 눌러라.</h1></Route>
+      </Switch>
+      <Footer />
+    </div>
+  )
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
