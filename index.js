485e0d2eaddd28d9300167c0322a0c21d1db9acb
JKInternational <93498814+JKInternational@users.noreply.github.com>
2021-11-22 09:23:43 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import './index.css';
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Introduce from "./components/Introduce";
-import Tichop from "./components/Tichop.js";
-import Worthytool from "./components/Worthytool.js";
+import Tichop from "./components/Tichop";
+import Worthytool from "./components/Worthytool";
 import Blueshark from "./components/Blueshark";
 import Etc from "./components/Etc";
 import Movie from "./components/Movie";
 import Notice from "./components/Notice";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -21,18 +21,18 @@ function App(){
   return (
     <div>
       <Navbar />
-      <Switch>
-        <Route exact path="/"><Main /></Route>
-        <Route path="/introduce"><Introduce /></Route>
-        <Route path="/tichop"><Tichop /></Route>
-        <Route path="/blueshark"><Blueshark /></Route>
-        <Route path="/worthytool"><Worthytool /></Route>
-        <Route path="/etc"><Etc /></Route>
-        <Route path="/movie"><Movie /></Route>
-        <Route path="/notice"><Notice /></Route>
-        <Route path="/contact"><Contact /></Route>
-        <Route path="/"><h1>정확히 좀 눌러라.</h1></Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/introduce" element={<Introduce />} />
+        <Route path="/tichop" element={<Tichop />} />
+        <Route path="/blueshark" element={<Blueshark />} />
+        <Route path="/worthytool" element={<Worthytool />} />
+        <Route path="/etc" element={<Etc />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<h1>정확히 좀 눌러라.</h1>} />
+      </Routes>
       <Footer />
     </div>
   )
