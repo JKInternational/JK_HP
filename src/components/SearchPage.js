e7d793f17ac303c25b33f1555279eec62d00c822
JKInternational <j-kinternational@naver.com>
2022-03-16 11:47:26 +0900

    searchPage에서 try catch 문제 해결

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -19,7 +19,7 @@ class SearchPage extends React.Component {
     var iserror = false;
 
     try {
-      console.log(this.props.params.query);
+      // console.log(this.props.params.query);
       const response = await axios.get(
         "http://jkintl.iptime.org:10337/api/items/?populate=*&_limit=-1&filters[$or][0][description][$containsi]=" +
           String(this.props.params.query) +
@@ -28,8 +28,8 @@ class SearchPage extends React.Component {
           "&filters[$or][2][mainDescription][$containsi]=" +
           String(this.props.params.query)
       );
-      console.log(response);
-      this.setState({ searchFunction: response });
+      // console.log(response);
+      // this.setState({ searchFunction: response });
     } catch (error) {
       iserror = true;
     }
@@ -37,6 +37,44 @@ class SearchPage extends React.Component {
       this.setState({
         searchFunction: { data: { data: [] } },
       });
+    }
+
+    try {
+      // console.log(this.props.params.query);
+      const response = await axios.get(
+        "http://jkintl.iptime.org:10337/api/items/?populate=*&_limit=-1&filters[$or][0][description][$containsi]=" +
+          String(this.props.params.query) +
+          "&filters[$or][1][name][$containsi]=" +
+          String(this.props.params.query) +
+          "&filters[$or][2][mainDescription][$containsi]=" +
+          String(this.props.params.query)
+      );
+
+      if (
+        response &&
+        response.data &&
+        response.data.data &&
+        response.data.data.map
+      ) {
+        var grouped = [];
+        var each = [];
+        for (var i = 0; i < response.data.data.length; ++i) {
+          if (i % 2 == 0) {
+            each = [response.data.data[i]];
+          } else {
+            each.push(response.data.data[i]);
+            grouped.push(each);
+            each = [];
+          }
+        }
+        if (each.length > 0) {
+          grouped.push(each);
+        }
+        response.data.data = grouped;
+      }
+      this.setState({ searchFunction: response });
+    } catch (error) {
+      this.setState({ error });
     }
   };
 
@@ -96,31 +134,38 @@ class SearchPage extends React.Component {
               {this.state.searchFunction.data &&
               this.state.searchFunction.data.data.map &&
               this.state.searchFunction.data.data.length > 0 ? (
-                this.state.searchFunction.data.data.map((item) => {
+                this.state.searchFunction.data.data.map((pairItem) => {
                   return (
-                    <ul className="container0">
-                      <Link
-                        to={"/detail/" + item.id}
-                        className="stuffBoxSwitch"
-                        href=""
-                      >
-                        <li id="stuffBox" style={stuffBox}>
-                          <p>
-                            <img
-                              className="stuffBoxImg"
-                              src={
-                                "http://jkintl.iptime.org:10337" +
-                                item.attributes.indexImage.data.attributes.url
-                              }
-                            />
-                          </p>
-                          <p id="stuffName">{item.attributes.name}</p>
-                          <p id="stuffSpec" style={textBox}>
-                            {item.attributes.mainDescription}
-                          </p>
-                        </li>
-                      </Link>
-                    </ul>
+                    <div class="stuffPairGroup">
+                      {pairItem.map((item) => {
+                        return (
+                          <ul className="container0">
+                            <Link
+                              to={"/detail/" + item.id}
+                              className="stuffBoxSwitch"
+                              href=""
+                            >
+                              <li id="stuffBox" style={stuffBox}>
+                                <p>
+                                  <img
+                                    className="stuffBoxImg"
+                                    src={
+                                      "http://jkintl.iptime.org:10337" +
+                                      item.attributes.indexImage.data.attributes
+                                        .url
+                                    }
+                                  />
+                                </p>
+                                <p id="stuffName">{item.attributes.name}</p>
+                                <p id="stuffSpec" style={textBox}>
+                                  {item.attributes.mainDescription}
+                                </p>
+                              </li>
+                            </Link>
+                          </ul>
+                        );
+                      })}
+                    </div>
                   );
                 })
               ) : (
