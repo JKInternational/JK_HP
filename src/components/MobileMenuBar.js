43f5466827c4f8ed7de9370f87f7e4f550314b96
JKInternational <j-kinternational@naver.com>
2022-04-11 14:44:30 +0900

    스크롤 라이브러리 변경

--- src/components/MobileMenuBar.js
+++ src/components/MobileMenuBar.js
@@ -2,45 +2,50 @@ import React from "react";
 import "./Main.css";
 // import CarouselMenu, { CarouselMenuItem } from "./CarouselMenu";
 import { Link } from "react-router-dom";
-import ScrollContainer from "react-indiana-drag-scroll";
-import Draggable from "react-draggable";
+// import ScrollContainer from "react-indiana-drag-scroll";
+// import Draggable from "react-draggable";
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
+import "./hideScrollbar.css";
+// import HorizontalScroll from "react-scroll-horizontal";
 
 class MobileMenuBar extends React.Component {
   render() {
     return (
       <>
-        <Draggable disabled={true}>
-          <div className="mobileMenuBar">
-            <ScrollContainer className="scrollContainer">
-              <ul className="menuBar">
-                <Link to="/introduce">
-                  <li>회사소개</li>
-                </Link>
-                <Link to="/tichoppower">
-                  <li>티찹전동공구</li>
-                </Link>
-                <Link to="/tichopcomp">
-                  <li>티찹컴프레서</li>
-                </Link>
-                <Link to="/blueshark">
-                  <li>블루샤크</li>
-                </Link>
-                <Link to="/worthytool">
-                  <li>워디툴</li>
-                </Link>
-                <Link to="/movie">
-                  <li>동영상리뷰</li>
-                </Link>
-                <Link to="/notice">
-                  <li>소식</li>
-                </Link>
-                <Link to="/contact">
-                  <li>찾아오시는길</li>
-                </Link>
-              </ul>
-            </ScrollContainer>
-          </div>
-        </Draggable>
+        {/* <Draggable disabled={true}> */}
+        <div className="mobileMenuBar">
+          {/* <ScrollContainer className="scrollContainer"> */}
+          <ScrollMenu>
+            <ul className="menuBar">
+              <Link to="/introduce">
+                <li>회사소개</li>
+              </Link>
+              <Link to="/tichoppower">
+                <li>티찹전동공구</li>
+              </Link>
+              <Link to="/tichopcomp">
+                <li>티찹컴프레서</li>
+              </Link>
+              <Link to="/blueshark">
+                <li>블루샤크</li>
+              </Link>
+              <Link to="/worthytool">
+                <li>워디툴</li>
+              </Link>
+              <Link to="/movie">
+                <li>동영상리뷰</li>
+              </Link>
+              <Link to="/notice">
+                <li>소식</li>
+              </Link>
+              <Link to="/contact">
+                <li>찾아오시는길</li>
+              </Link>
+            </ul>
+          </ScrollMenu>
+          {/* </ScrollContainer> */}
+        </div>
+        {/* </Draggable> */}
       </>
     );
   }
