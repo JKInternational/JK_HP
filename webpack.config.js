e76e60209a9c5947bf8f0b27b8cb0ee1a36c01bc
JKInternational <j-kinternational@naver.com>
2024-12-11 16:46:35 +0900

    webpack 위치 이동

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    allowedHosts: "all", // 모든 호스트 허용
+    host: "0.0.0.0", // 외부에서 접근 가능하도록 설정
+    port: 3000, // 필요한 포트 번호로 설정
+    disableHostCheck: true, //Invaild Host header 오류 수정 위해 추가
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
