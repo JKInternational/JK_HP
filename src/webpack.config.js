ea22bc620977608c9ddf39ae2135068823d85940
JKInternational <j-kinternational@naver.com>
2024-12-11 15:58:56 +0900

    웹팩 수정. 뭘 이렇게 만져가지고는 Invalid Host header를 경험하는가.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    allowedHosts: "all", // 모든 호스트 허용
+    host: "0.0.0.0", // 외부에서 접근 가능하도록 설정
+    port: 3000, // 필요한 포트 번호로 설정
+  },
   module: {
     rules: [
       {
